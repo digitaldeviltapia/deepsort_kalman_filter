292a88ead28bca25738722fa357d2bf057edde48
Tapia <mario.tapia@cimat.mx>
2025-06-05 19:22:22 -0600

    se implementó el filtro de Kalman

--- main.py
+++ main.py
@@ -7,25 +7,26 @@ from ultralytics import YOLO
 from tracker import Tracker
 
 
-video_path = os.path.join('.', 'data', 'people.mp4')
+video_path = os.path.join('people.mp4')
 video_out_path = os.path.join('.', 'out.mp4')
 
 cap = cv2.VideoCapture(video_path)
 ret, frame = cap.read()
 
-cap_out = cv2.VideoWriter(video_out_path, cv2.VideoWriter_fourcc(*'MP4V'), cap.get(cv2.CAP_PROP_FPS),
-                          (frame.shape[1], frame.shape[0]))
+#cap_out = cv2.VideoWriter(video_out_path, cv2.VideoWriter_fourcc(*'MP4V'), cap.get(cv2.CAP_PROP_FPS),
+#                          (frame.shape[1], frame.shape[0]))
 
 model = YOLO("yolov8n.pt")
 
 tracker = Tracker()
 
-colors = [(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for j in range(10)]
+colors = [(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for j in range(100)]
+box_color = box_color = (255, 255, 0)  
 
 detection_threshold = 0.5
 while ret:
 
-    results = model(frame)
+    results = model(frame, device='cpu')
 
     for result in results:
         detections = []
@@ -47,10 +48,14 @@ while ret:
             track_id = track.track_id
 
             cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (colors[track_id % len(colors)]), 3)
+            cv2.putText(frame, f'Track ID: {track_id}', (int(x1), int(y1) - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
-    cap_out.write(frame)
+    #cap_out.write(frame)
+    cv2.imshow('frame', frame)
+    key = cv2.waitKey(20) 
     ret, frame = cap.read()
 
 cap.release()
-cap_out.release()
+#cap_out.release()
 cv2.destroyAllWindows()
